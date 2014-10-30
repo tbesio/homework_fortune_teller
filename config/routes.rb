a8b1b82061b7ae52602cfef2f756f00a30f200f5
Tom Besio <thomas.besio@gmail.com>
2014-10-29 23:01:27 -0500

    Fixed /zodiacs/pisces
    Fixed the route in routes.rb and added the fish.html.erb view template

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get("/zodiacs/sagittarius", { :controller =>  "zodiacs",  :action =>  "archer" })
   get("/zodiacs/capricorn",   { :controller =>  "zodiacs",  :action =>  "goat" })
   get("/zodiacs/aquarius",    { :controller =>  "zodiacs",  :action =>  "waterbearer" })
-  # get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
+  get("/zodiacs/pisces",      { :controller =>  "zodiacs",  :action =>  "fish" })
 
 
   # PART 2: VARIABLE ROUTES
