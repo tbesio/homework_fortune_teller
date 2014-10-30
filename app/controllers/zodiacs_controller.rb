60a148b54be628fd6d34aa37a0cbf45da478f8ac
Tom Besio <thomas.besio@gmail.com>
2014-10-29 23:24:51 -0500

    Beginning Dynamic Routes
    Added the dynamic route :the_sign

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -70,4 +70,10 @@ class ZodiacsController < ApplicationController
 
     render 'fish'
   end
+
+  def sign
+    # @prediction = "Business and financial success makes you happy and satisfied, Pisces. You also look forward to moving on. The downside of this flush period is that people who aren't particularly trustworthy might decide to latch onto your coattails for their own purposes. Some might even ask for loans. Be careful about the ones you assist. They might not be honest with you. Don't fall for any sob stories."
+
+    render 'sign'
+  end
 end
