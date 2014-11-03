97dceb32c1c20723e2906b8b13bd607e5ba5e8b1
Tom Besio <thomas.besio@gmail.com>
2014-11-03 09:31:26 -0600

    creatures/:the_creature
    Per the read_me, uncommented the route, wrote a new action ‘creature’
    in controller zodiacs to handle, and created a creature view for
    output. Syntax was similar to the sign action but the zodiac instance
    was created as a lookup using the creature and @sign and @prediction
    were defined from attributes of this_zodiac

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -72,10 +72,15 @@ class ZodiacsController < ApplicationController
   end
 
   def sign
-    # @prediction = "Business and financial success makes you happy and satisfied, Pisces. You also look forward to moving on. The downside of this flush period is that people who aren't particularly trustworthy might decide to latch onto your coattails for their own purposes. Some might even ask for loans. Be careful about the ones you assist. They might not be honest with you. Don't fall for any sob stories."
-
     this_zodiac = Zodiac.find_by({ :sign => params["the_sign"]})
     @prediction = this_zodiac.fortune
     render 'sign'
+  end
+
+  def creature
+    this_zodiac = Zodiac.find_by({ :creature => params["the_creature"]})
+    @sign = this_zodiac.sign
+    @prediction = this_zodiac.fortune
+    render 'creature'
   end
 end
